06df28dd384bf833f99d84f014820693bf3a5c0d
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-10-22 19:28:04 +0900

    Title: 369 게임, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251022/369 게임/369-game.py
+++ 251022/369 게임/369-game.py
@@ -1,14 +1,11 @@
-N = input()
-n = int(N)
+N = int(input())
 i = 1
-while i <= n:
+while i <= N:
     flag = False
-    ten = 1
-    for j in range(0, len(N)):
-        if i % ten in {3, 6, 9}:
+    for j in range(0, len(str(i))):
+        if str(i)[j] in {'3', '6', '9'}:
             flag = True
             break
-        ten *= 10
 
     if i % 3 == 0 or flag:
         print(0, end=' ')
