61d536259f1b96c787b67c0bc2572b1e43afc471
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-10-22 19:26:08 +0900

    Title: 369 게임, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251022/369 게임/369-game.py
+++ 251022/369 게임/369-game.py
@@ -1,7 +1,16 @@
-N = int(input())
+N = input()
+n = int(N)
 i = 1
-while i <= N:
-    if i % 3 == 0 or i % 10 in {3, 6, 9} or i / 10 in {3, 6, 9}:
+while i <= n:
+    flag = False
+    ten = 1
+    for j in range(0, len(N)):
+        if i % ten in {3, 6, 9}:
+            flag = True
+            break
+        ten *= 10
+
+    if i % 3 == 0 or flag:
         print(0, end=' ')
     else:
         print(i, end=' ')
