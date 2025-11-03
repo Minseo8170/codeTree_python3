9e0cefa006e44a6ca4d92cb17cecb869d0a5f01e
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-11-03 23:38:13 +0900

    Title: 별표 출력하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251103/별표 출력하기/print-star.py
@@ -0,0 +1,9 @@
+N = int(input())
+for i in range(1, N+1):
+    for _ in range(i):
+        print('*', end=' ')
+    print('')
+for i in range(N-1, 0, -1):
+    for _ in range(i):
+        print('*', end=' ')
+    print('')
