a9f217770258fd4014117f9789b91658fcafda9d
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-11-05 19:32:23 +0900

    Title: 체크 다이아몬드, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251105/체크 다이아몬드/check-diamond.py
+++ 251105/체크 다이아몬드/check-diamond.py
@@ -1,19 +1,5 @@
 n = int(input())
-for i in range(n):
-    for _ in range(n-i-1):
-        print(' ', end='')
-    for j in range(i*2+1):
-        if j % 2 == 1:
-            print(' ', end='')
-        else:
-            print('*', end='')
-    print()
-for i in range(n-1):
-    for _ in range(i+1):
-        print(' ', end='')
-    for j in range((n-i-1)*2-1):
-        if j % 2 == 1:
-            print(' ', end='')
-        else:
-            print('*', end='')
-    print()
+for i in range(1, n+1):
+    print(' ' * (n-i), '* ' * i, sep='')
+for i in range(n-1, 0, -1):
+    print(' ' * (n-i), '* ' * i, sep='')
