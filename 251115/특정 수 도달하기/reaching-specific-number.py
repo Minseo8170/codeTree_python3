21613c8e409c2c66500dc38196cb661007ab9380
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-11-15 13:17:08 +0900

    Title: 특정 수 도달하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251115/특정 수 도달하기/reaching-specific-number.py
+++ 251115/특정 수 도달하기/reaching-specific-number.py
@@ -1,7 +1,9 @@
 n = list(map(int, input().split()))
 total = 0
+cnt = 0
 for i in range(10):
     if n[i] >= 250:
         break;
+    cnt += 1
     total += n[i]
-print(f"{total} {total/i:.1f}")
+print(f"{total} {total/cnt:.1f}")
