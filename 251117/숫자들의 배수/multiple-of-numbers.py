b5dc7088e8dcb102ecc02e07d6547617b308e081
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-11-17 04:26:11 +0900

    Title: 숫자들의 배수, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251117/숫자들의 배수/multiple-of-numbers.py
+++ 251117/숫자들의 배수/multiple-of-numbers.py
@@ -1,11 +1,9 @@
 n = int(input())
-fin = False
-if n == 5:
-    print(5, 10)
-else:
-    for i in range(n, n * 10 + 1, n):
-        if fin and i % 5 == 0:
-            break
-        print(i, end=' ')
-        if i % 5 == 0:
-            fin == True
+arr = [i for i in range(n, n * 10 + 1, n)]
+flag = False
+for i in arr:
+    print(i, end=' ')
+    if not flag and i % 5 == 0:
+        flag = True
+    elif flag and i % 5 == 0:
+        break
