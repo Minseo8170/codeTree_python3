66db8514ce77ae031e44cb59d00e2ff301fa28d9
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-11-22 18:47:57 +0900

    Title: 정수 직사각형, Time: 65ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 251122/정수 직사각형/rectangle-of-numbers.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+arr = [
+    [i + j * 3 for i in range(1, m+1)]
+    for j in range(n)
+]
+
+for i in range(n):
+    for j in range(m):
+        print(arr[i][j], end=' ')
+    print()
