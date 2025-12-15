531eed85d1bf83a80a2895b058eabeae63548730
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-12-15 13:16:44 +0900

    Title: 최대공약수 구하기, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251215/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 251215/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -3,12 +3,11 @@ n, m = map(int, input().split())
 # Please write your code here.
 def min_num(n, m):
     cnt = 0
-    for i in range(1, n//2 + 1):
+    for i in range(1, 101):
+        if i >= n or i >= m:
+            break
         if n % i == 0 and m % i == 0:
             cnt = i
     return cnt
 
-if n > m:
-    print(min_num(m, n))
-else:
-    print(min_num(n, m))
+print(min_num(n, m))
